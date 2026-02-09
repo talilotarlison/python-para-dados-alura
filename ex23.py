c022d3345546d510bba5fac8716d2fb9d64097ce
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2026-02-09 10:30:08 -0300

    desafios modulo 1

---
+++ ex23.py
@@ -0,0 +1,4 @@
+from random import choice
+
+lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+print(f"Número escolhido: {choice(lista)}")
---
+++ ex25.py
@@ -0,0 +1,5 @@
+from math import pow
+
+base = int(input("Digite a base da potência: "))
+expoente = int(input("Digite o expoente da potência: "))
+print(f"{base} elevado a {expoente} é igual a {pow(base, expoente)}")
