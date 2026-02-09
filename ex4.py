821eaa209f23e5394e330fb52dd3352f38a36fd4
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2026-02-09 09:11:18 -0300

    atividade

---
+++ ex4.py
@@ -0,0 +1,6 @@
+
+def cal_notas():
+    media = (5.0 + 9.0 + 3.3)/3
+    print(media)
+
+cal_notas()
---
+++ ex5.py
@@ -0,0 +1,20 @@
+notas = [ 10.0 , 5.0 , 3.0 ]
+
+# a nossa função recebe uma lista do tipo list e retorna uma variável do tipo float
+def media(lista: list) -> float:
+  calculo = sum(lista) / len(lista)
+  return calculo
+
+def status_aprovacao(nota_final):
+    if nota_final >= 7:
+        print(f'Aprovado com sucesso. \nNotas final: {nota_final} ')
+    else:
+        print(f'Reprovado. \nNotas final: {nota_final} ')    
+
+def cal_notas(notas: list)->None:
+    calc = media(notas)
+    status_aprovacao(calc)
+
+cal_notas(notas)
+
+
