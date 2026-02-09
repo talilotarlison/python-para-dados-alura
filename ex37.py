2e625568301ba8dffe36168970d4181480ba7005
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2026-02-09 18:58:12 -0300

    zip

---
+++ ex37.py
@@ -0,0 +1,8 @@
+tupla_iteravel = [('J392', 'João'), ('M890', 'Maria'), ('J681', 'José'), ('C325', 'Claúdia'), ('A49', 'Ana')]
+ids, nomes  = zip(*tupla_iteravel)
+
+ids = list(ids)
+nomes = list(nomes)
+
+print("IDs = ", ids)
+print("Nomes = ", nomes)
